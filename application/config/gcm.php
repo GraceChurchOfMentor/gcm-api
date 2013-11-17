d2687c073b904f8d33c9438dbd4e458f29828ede
Nick Bair <njbair@gmail.com>
2013-11-17 13:36:49 -0800

    little work on bible

--- application/config/gcm.php
+++ application/config/gcm.php
@@ -1,9 +1,12 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['cache_default_driver'] = 'file';
+$config['cache_default_driver'] = 'dummy';
 
 # the default number of events to display
 $config['events_default_count'] = 4;
 
 # default length of time after start date, in strtotime() format
 $config['events_default_timeframe'] = '+ 1 month';
+
+# the default bible reference to retrieve, if none given
+$config['bible_default_reference'] = 'John 3:16';
