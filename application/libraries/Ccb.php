dce1e4ec3fb4b85285088ff6dbe79ed4f739fb6d
Nick Bair <njbair@gmail.com>
2013-11-17 14:03:01 -0800

    Removed CI dependencies from CCB library

--- application/libraries/Ccb.php
+++ application/libraries/Ccb.php
@@ -1,28 +1,35 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
 class Ccb
 {
-	function __construct()
+	function __construct($params)
 	{
-		$this->CI =& get_instance();
-		$this->CI->config->load('ccb');
-		$this->CI->load->library('utils');
+		$this->base_url = $params['ccb_base_url'];
+		$this->username = $params['ccb_username'];
+		$this->password = $params['ccb_password'];
+		$this->timeout  = isset( $params['ccb_timeout'] ) ? $params['ccb_timeout'] : 120;
 	}
 
 	public function get($srv, $args=FALSE)
 	{
 		$args || $args = array();
 		$args['srv'] = $srv;
+		$args && ($args = '?' . http_build_query($args)) || $args = '';
 
-		$response = $this->CI->utils->cURL(
-			$this->CI->config->item('ccb_base_url'),
-			$args,
-			$this->CI->config->item('ccb_username'),
-			$this->CI->config->item('ccb_password'),
-			$this->CI->config->item('ccb_timeout')
-		);
+		$ch = curl_init();
 
-		$object = $this->xml_to_object($response->data);
+		curl_setopt($ch, CURLOPT_URL, $this->base_url . $args);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, "$this->username:$this->password");
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+
+		$data = curl_exec($ch);
+		$info = curl_getinfo($ch);
+
+		curl_close($ch);
+
+		$object = $this->xml_to_object($data);
 
 		return $object;
 	}
