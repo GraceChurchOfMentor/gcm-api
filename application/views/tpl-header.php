7bf70762d3014f5564ef6785cb643c1ab92d7943
Nick Bair <njbair@gmail.com>
2013-11-27 23:01:03 -0500

    comments removed

--- application/views/tpl-header.php
+++ application/views/tpl-header.php
@@ -4,8 +4,6 @@
 		<meta charset="utf-8" />	
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<title><?php echo $title; ?></title>
-		<!--<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.3/normalize.min.css" />-->
-		<!--<link href='//fonts.googleapis.com/css?family=Source+Code+Pro:300|Roboto:300,300italic,500' rel='stylesheet' type='text/css'>-->
 		<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/style.css'); ?>" />
 	</head>
 	<body>
