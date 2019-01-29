ab26ca7da38654d04897dc37881b209fbfdff9fe
Nick Bair <njbair@gmail.com>
2019-01-29 13:30:34 -0500

    remove personal contact details

--- application/views/events_html.php
+++ application/views/events_html.php
@@ -1,27 +1,30 @@
 <ul class="events-wrapper">
-	<?php foreach ($items as $item): ?>
-		<li class="date">
-			<h4 class="date-header"><?php echo $item['date_string']; ?></h4>
-			<ul class="events">
-				<?php foreach ($item['events'] as $event): ?>
-					<li class="event">
-						<span class="event-name"><?php echo $event['event_name']; ?></span>
-						<?php if ($event['short_time_string'] !== ''): ?>
-							<span class="event-time"><?php echo $event['short_time_string']; ?></span>
-						<?php endif; ?>
-						<?php if ($details): ?>
-							<div class="event-details">
-								<p><?php echo $event['event_description']; ?></p>
-								<p>
-									<?php echo $event['time_string']; ?><br/>
-									Contact <a href="mailto:<?php echo $event['leader_email']; ?>"><?php echo $event['leader_name']; ?></a><br/>
-									<?php echo $event['leader_phone']; ?>
-								</p>
-							</div>
-						<?php endif; ?>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</li>
-	<?php endforeach; ?>
+    <?php foreach ($items as $item): ?>
+        <li class="date">
+            <h4 class="date-header"><?php echo $item['date_string']; ?></h4>
+            <ul class="events">
+                <?php foreach ($item['events'] as $event): ?>
+                    <li class="event">
+                        <span class="event-name"><?php echo $event['event_name']; ?></span>
+                        <?php if ($event['short_time_string'] !== ''): ?>
+                            <span class="event-time"><?php echo $event['short_time_string']; ?></span>
+                        <?php endif; ?>
+                        <?php if ($details): ?>
+                            <div class="event-details">
+                                <p><?php echo $event['event_description']; ?></p>
+                                <p>
+                                    <?php echo $event['time_string']; ?><br/>
+<?php /*
+                                    Contact <a href="mailto:<?php echo $event['leader_email']; ?>"><?php echo $event['leader_name']; ?></a><br/>
+                                    <?php echo $event['leader_phone']; ?>
+ */ ?>
+                                    Contact <?php echo $event['leader_name']; ?>
+                                </p>
+                            </div>
+                        <?php endif; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </li>
+    <?php endforeach; ?>
 </ul>
