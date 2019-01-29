e37d2b5b81edd80415cc2a63e66f0e9151173808
Nick Bair <njbair@gmail.com>
2019-01-29 13:24:07 -0500

    change cache type

--- application/config/gcm.php
+++ application/config/gcm.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['cache_default_driver'] = 'apc';
+$config['cache_default_driver'] = 'file';
 
 # the default number of events to display
 $config['events_default_count'] = 4;
